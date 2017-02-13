af478e30c5005de24e2c8cfe35ef4d8322b7cda6
Chaitanya Deshpande <chaitanya.deshp@gmail.com>
2017-02-13 18:31:19 +0530

    Update ADB1.3.cpp

--- ADB1.3.cpp
+++ ADB1.3.cpp
@@ -6,10 +6,10 @@ using namespace std;
 class device
 {
 	private:
-		char bcho,bloc[50], bstr[100]="adb backup ",rfile[100],rstr[115]="adb restore ";
+		
 	public:
     void devchk()
-	{
+	{	
 	cout<<"Cheaking Device Connected..\n";
 	system("adb wait-for-device");
 	cout<<"Device found!\n";
@@ -17,6 +17,7 @@ class device
 	}	
 	void backup()
 	{
+		char bcho,bloc[50], bstr[100]="adb backup ";
 		system("cls");
 		cout<<"Running Backup Module..\n\n";
 		cout<<"Enter location Of Backup File\n\n";
@@ -46,6 +47,7 @@ class device
 	}
 	void restore()
 	{
+		char rfile[100],rstr[115]="adb restore ";
 		cout<<"Running Restore Module..\n\n";
 		cout<<"Enter Path For Backup File: ";
 		cin>>rfile;
@@ -53,6 +55,35 @@ class device
 		puts(rstr);
 		getch();
 		system(rstr);
+	}
+	void rreboot()
+	{
+		system("adb reboot recovery");
+	}
+	void breboot()
+	{
+		system("adb reboot bootloader");
+		system("fastboot devices");
+		system("fastboot flashing get_unlock_ability");
+	}
+	void install()
+	{
+		char str[100]="adb install",cho,apkloc[100];
+		cout<<"Do You Want To Install On SD-Card? [Y/N]";
+		cin>>cho;
+		if(cho=='Y'||cho=='y')
+		{
+			strcat(str," -s");
+		}
+		cout<<"Give All Runtime Permissions? [Y/N]";
+		cin>>cho;
+		if(cho=='Y'||cho=='y')
+		{
+			strcat(str," -g");
+		}
+		cout<<"Enter Location Of APK: ";
+		cin>>apkloc;
+		strcat(str,apkloc);
 	}
 	
 };

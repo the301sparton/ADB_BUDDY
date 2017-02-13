264f861a6d9b9112ec194487fa6f7f3aa9b257dc
Chaitanya Deshpande <chaitanya.deshp@gmail.com>
2017-02-13 18:32:59 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
+#include <conio.h>
 #include <stdlib.h>
-#include <"ADB1.3.cpp">
+#include <C:\Users\Jenkin\Documents\GitHub\ADB_BUDDY\ADB_BUDDY-master\ADB1.3.cpp>
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 void getcho()
@@ -9,6 +10,9 @@ void getcho()
 		int cho;
 		cout<<"Press 1 For Backup\n";
 		cout<<"Press 2 For Restore\n";
+		cout<<"Press 3 To Reboot In Recovery\n";
+		cout<<"Press 4 To Reboot In Bootloader\n";
+		cout<<"Press 5 To Install A Package\n";
 		cout<<"Choice: ";
 		cin>>cho;
 		if(cho==1)
@@ -19,6 +23,18 @@ void getcho()
 		{
 			plus.restore();
 		}
+		else if(cho==3)
+		{
+			plus.rreboot();
+		}
+		else if(cho==4)
+		{
+			plus.breboot();
+		}
+		else if(cho==5)
+		{
+			plus.install();
+		}
 		else
 		{
 			exit(1);
@@ -27,7 +43,12 @@ void getcho()
 int main(int argc, char** argv)
 {
 	device test;
+	while(1)
+	{
+	system("cls");
 	test.devchk();
-	getcho();
+	getcho();	
+	}
 	return 0;
 }
+ 

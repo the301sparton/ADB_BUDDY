0036b8b18bf8c468934074ab8717b3b45fb12359
Chaitanya Deshpande <chaitanya.deshp@gmail.com>
2017-03-23 20:40:38 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <conio.h>
 #include <stdlib.h>
+#include <C:\Users\Jenkin\Documents\GitHub\ADB_BUDDY\ADB_BUDDY-master\fasthandler.cpp>
 #include <C:\Users\Jenkin\Documents\GitHub\ADB_BUDDY\ADB_BUDDY-master\ADB1.3.cpp>
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
 void getcho()
 	{
 		device plus;
@@ -11,7 +13,7 @@ void getcho()
 		cout<<"Press 1 For Backup\n";
 		cout<<"Press 2 For Restore\n";
 		cout<<"Press 3 To Reboot In Recovery\n";
-		cout<<"Press 4 To Reboot In Bootloader\n";
+		cout<<"Press 4 To Start Fastboot Mode\n";
 		cout<<"Press 5 To Install A Package\n";
 		cout<<"Press 6 To Push Files\n";
 		cout<<"Press 7 To Pull Files\n";
@@ -32,7 +34,8 @@ void getcho()
 		}
 		else if(cho==4)
 		{
-			plus.breboot();
+            		fastboot_device plus;
+			plus.fastboot_mode();   //fastboot mode
 		}
 		else if(cho==5)
 		{
